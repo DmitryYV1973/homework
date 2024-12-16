51a5c2da6299a3754abd34ee36fbfcb2ee1f0639
Dmitry Yudin <yudin.dm2013@gmail.com>
2024-12-17 01:53:12 +0300

    Добавлены 2 функции записи и добавления данных в JSON-файл

--- files_utils.py
+++ files_utils.py
@@ -5,3 +5,13 @@ def read_json(file_path: str, encoding: str = "utf-8"):
     with open(file_path, "r", encoding=encoding) as f:
         json_content = f.read()
         return json_content
+    
+
+def write_json(data, file_path: str, encoding: str = "utf-8"):
+    with open(file_path, "w", encoding=encoding) as f:
+        f.write(data)
+
+
+def append_json(data: list[dict], file_path: str, encoding: str = "utf-8"):
+    with open(file_path, "a", encoding=encoding) as f:
+        f.write(data)
