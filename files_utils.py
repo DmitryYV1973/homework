d772c273395723bf0476ca9e6833329d64cc3453
Dmitry Yudin <yudin.dm2013@gmail.com>
2024-12-19 05:40:25 +0300

    Добавлены 2 функции для работы с CSV

--- files_utils.py
+++ files_utils.py
@@ -6,12 +6,26 @@ def read_json(file_path: str, encoding: str = "utf-8"):
         json_content = f.read()
         return json_content
     
-
+# Функция для записи данных в файл в формате JSON
 def write_json(data, file_path: str, encoding: str = "utf-8"):
     with open(file_path, "w", encoding=encoding) as f:
         f.write(data)
 
-
+# Функция добавления данных в файл в формате JSON
 def append_json(data: list[dict], file_path: str, encoding: str = "utf-8"):
     with open(file_path, "a", encoding=encoding) as f:
         f.write(data)
+
+# Функции для работы с CSV
+def write_csv(data, file_path, delimiter=';', encoding: str ='windows-1251')
+    with open(file_path, 'w', encoding=encoding) as f:
+        writer = csv.writer(f, delimiter=delimiter)
+        writer.writerows(data)
+
+
+def append_csv(data, file_path, delimiter=';', encoding: str ='windows-1251')
+    with open(file_path, 'a', encoding=encoding) as f:
+        writer = csv.writer(f, delimiter=delimiter)
+        writer.writerows(data)
+
+
