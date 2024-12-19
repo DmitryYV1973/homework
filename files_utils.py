9eead9374f68fd5db876079c478bde68caadc522
Dmitry Yudin <yudin.dm2013@gmail.com>
2024-12-19 05:55:57 +0300

    Исправлены ошибки в функциях для работы с CSV и добавлены функции для работы с TXT

--- files_utils.py
+++ files_utils.py
@@ -17,15 +17,29 @@ def append_json(data: list[dict], file_path: str, encoding: str = "utf-8"):
         f.write(data)
 
 # Функции для работы с CSV
-def write_csv(data, file_path, delimiter=';', encoding: str ='windows-1251')
+def write_csv(data, file_path, delimiter=';', encoding: str ='windows-1251'):
     with open(file_path, 'w', encoding=encoding) as f:
-        writer = csv.writer(f, delimiter=delimiter)
+        writer = f.writer(f, delimiter=delimiter)
         writer.writerows(data)
 
 
-def append_csv(data, file_path, delimiter=';', encoding: str ='windows-1251')
+def append_csv(data, file_path, delimiter=';', encoding: str ='windows-1251'):
     with open(file_path, 'a', encoding=encoding) as f:
-        writer = csv.writer(f, delimiter=delimiter)
+        writer = f.writer(f, delimiter=delimiter)
         writer.writerows(data)
 
+# Функции для работы с TXT
+def read_txt(file_path, encoding: str = "utf-8"):
+    with open(file_path, 'r', encoding=encoding) as f:
+        content = f.read()
+        return content
 
+
+def write_txt(data, file_path, encoding: str = "utf-8"):
+    with open(file_path, 'w', encoding=encoding) as f:
+        f.write(data)
+
+
+def append_txt(data, file_path, encoding: str = "utf-8"):
+    with open(file_path, 'a', encoding=encoding) as f:
+        f.write(data)
