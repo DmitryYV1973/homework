6247491688bc2cb55a1221671e565409d74a1805
Dmitry Yudin <yudin.dm2013@gmail.com>
2025-02-22 13:49:43 +0300

    Выборка 2. "Герои с тайной идентичностью и необычными глазами"

--- HW35/hw_35.sql
+++ HW35/hw_35.sql
@@ -5,3 +5,12 @@ WHERE year BETWEEN 1990 AND 1999
     AND HAIR = 'bald'
     OR ALIGN = 'Bad Characters'
 LIMIT 94;
+
+
+-- Герои с тайной идентичностью и необычными глазами
+SELECT name, FIRST_APPEARANCE, eye
+FROM MarvelCharacters
+WHERE identify IS NOT NULL
+    AND eye NOT IN ('blue', 'brown', 'green')
+    AND FIRST_APPEARANCE IS NOT NULL
+LIMIT 1027;
