187b4cf605fd5116c4a90d706a803cef600f35ca
Dmitry Yudin <yudin.dm2013@gmail.com>
2025-03-09 13:24:07 +0300

    Добавлен класс для работы с файлами CSV и исправлены ошибки в классах TextFile и JSONFile

--- HW30/file_classes.py
+++ HW30/file_classes.py
@@ -1,5 +1,6 @@
 import abc
 import json
+import csv
 
 class AbstractFile(abc.ABC):
     '''Абстрактный класс для работы с файлами'''
@@ -25,16 +26,19 @@ class AbstractFile(abc.ABC):
             self.file_path = file_path
 
         def read(self) -> dict:
-            with open(self.file_path, r) as file:
+            with open(self.file_path, 'r') as file:
                 return json.load(file)
             
         def write(self, data) -> None:
-            with open(self.file_path, w) as file:
+            with open(self.file_path, 'w') as file:
                 json.dump(data, file)
 
         def append(self, data) -> None:
             current_data = self.read()
-            current_data.append(data)
+            if isinstance(current_data, list):
+                current_data.append(data)
+            else:
+                raise ValueError('Данные в файле должны быть в формате списка')
             self.write(current_data)
 
 
@@ -44,13 +48,34 @@ class TextFile(AbstractFile):
         self.file_path = file_path
 
     def read(self) -> str:
-        with open(self.file_path, r) as file:
+        with open(self.file_path, 'r') as file:
             return file.read()
         
     def write(self, data) -> None:
-        with open(self.file_path, w) as file:
+        with open(self.file_path, 'w') as file:
             file.write(data)
 
     def append(self, data) -> None:
-        with open(self.file_path, a) as file:
+        with open(self.file_path, 'a') as file:
             file.write(data)
+
+
+class CSVFile(AbstractFile):
+    '''Класс для работы с файлами в формате CSV'''
+    def __init__(self, file_path) -> None:
+        self.file_path = file_path
+
+        def read(self) -> list:
+            with open(self.file_path, 'r') as file:
+                reader = csv.reader(file)
+                return list(reader)
+            
+        def write(self, data) -> None:
+            with open(self.file_path, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(data)
+
+        def append(self, data) -> None:
+            with open(self.file_path, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(data)
